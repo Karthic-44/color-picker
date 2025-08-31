9326f7ad3e2903d3dff55a8a0cdf297457720817
Karthic <karthics004@gmail.com>
2025-08-31 10:59:24 +0530

    Added event handler and listener

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [backgroundColor, setBachgroundColor] = useState('#ffffff');
-  const colors = ['#ff0000'];
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+  const colors = ['#ff0000', '#2761F5', '#E5F01D', '#23f110ff', '#ee0feeff', '#000000ff', '#ffffffff'];
+
+  const handleColorChange = (color) => {
+    setBackgroundColor(color);    
+  };
 
   return (
-    <div>
+    <div style={{backgroundColor}}>
       <h1>Color Picker</h1>
       <div className='color-palette'>
         {colors.map((color,index) =>
         (
-           <div key={index} className='color-box' style={{backgroundColor:color}} onClick={() => {}}>
+           <div 
+            key={index} 
+            className='color-box' 
+            style={{backgroundColor:color}} 
+            onClick={() => handleColorChange(color)}>
 
            </div>    
         ))}
