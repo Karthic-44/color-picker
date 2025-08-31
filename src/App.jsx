43b6dd0f0755fdd388e637195e9f87c5517a34c7
Karthic <karthics004@gmail.com>
2025-08-31 10:47:54 +0530

    Added red color

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [backgroundColor, setBachgroundColor] = useState('#ffffff');
+  const colors = ['#ff0000'];
 
   return (
     <div>
       <h1>Color Picker</h1>
+      <div className='color-palette'>
+        {colors.map((color,index) =>
+        (
+           <div key={index} className='color-box' style={{backgroundColor:color}} onClick={() => {}}>
+
+           </div>    
+        ))}
+      </div>
     </div>
   );
 }
